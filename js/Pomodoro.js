fe5971d293e607e7886d7de57d758879738a9ceb
Your Name <you@example.com>
2023-09-18 15:06:18 +0200

    ajout du minuteur avec bouton de départ et réinitialisation

--- js/Pomodoro.js
+++ js/Pomodoro.js
@@ -1,20 +1,32 @@
+let departMinutes = 25;
+        let temps = departMinutes * 60;
+        let timerInterval; // Cette variable stockera l'ID de l'intervalle
 
+        const timerElement = document.getElementById("timer");
 
-const departMinutes = 5
-let temps = departMinutes * 60
+        function start() {
+            // Si un intervalle précédent est en cours, on le supprime
+            clearInterval(timerInterval);
 
-const timerElement = document.getElementById("timer")
+            // Réinitialise le temps à sa valeur initiale
+            temps = departMinutes * 60;
 
-setInterval(() => {
-  let minutes = parseInt(temps / 60, 10)
-  let secondes = parseInt(temps % 60, 10)
+            timerInterval = setInterval(() => {
+                let minutes = parseInt(temps / 60, 10);
+                let secondes = parseInt(temps % 60, 10);
 
-  minutes = minutes < 10 ? "0" + minutes : minutes
-  secondes = secondes < 10 ? "0" + secondes : secondes
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                secondes = secondes < 10 ? "0" + secondes : secondes;
 
-  timerElement.innerText = `${minutes}:${secondes}`
-  temps = temps <= 0 ? 0 : temps - 1
-}, 1000)
+                timerElement.innerText = `${minutes}:${secondes}`;
+                temps = temps <= 0 ? 0 : temps - 1;
+
+                if (temps === 0) {
+                    // Arrêter l'intervalle lorsque le temps est écoulé
+                    clearInterval(timerInterval);
+                }
+            }, 1000);
+          }
 
 
 
