27483dfd9cbe5da0c4511fd94a24282d596456cf
Cureau <baptiste.cureau@etu.unicaen.fr>
2023-09-18 16:46:37 +0200

    fin de la V1 sans le style

--- js/Pomodoro.js
+++ js/Pomodoro.js
@@ -1,34 +1,39 @@
-let departMinutes = 25;
-        let temps = departMinutes * 60;
-        let timerInterval; // Cette variable stockera l'ID de l'intervalle
+let departMinutes = 0.1;
+let temps = departMinutes * 60;
+let timerInterval; // Cette variable stockera l'ID de l'intervalle
+let boucle = 0;
 
-        const timerElement = document.getElementById("timer");
+const timerElement = document.getElementById("timer");
 
-        function start() {
-            // Si un intervalle précédent est en cours, on le supprime
+function start() {
+    // Si un intervalle précédent est en cours, on le supprime
+    clearInterval(timerInterval);
+
+    // Réinitialise le temps à sa valeur initiale
+    temps = departMinutes * 60;
+
+    timerInterval = setInterval(() => {
+        let minutes = parseInt(temps / 60, 10);
+        let secondes = parseInt(temps % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        secondes = secondes < 10 ? "0" + secondes : secondes;
+
+        timerElement.innerText = `${minutes}:${secondes}`;
+        temps = temps <= 0 ? 0 : temps - 1;
+        console.log(boucle);
+        if (temps === 0 && boucle < 1) {
             clearInterval(timerInterval);
-
-            // Réinitialise le temps à sa valeur initiale
+            departMinutes = 0.1;
             temps = departMinutes * 60;
-
-            timerInterval = setInterval(() => {
-                let minutes = parseInt(temps / 60, 10);
-                let secondes = parseInt(temps % 60, 10);
-
-                minutes = minutes < 10 ? "0" + minutes : minutes;
-                secondes = secondes < 10 ? "0" + secondes : secondes;
-
-                timerElement.innerText = `${minutes}:${secondes}`;
-                temps = temps <= 0 ? 0 : temps - 1;
-
-                if (temps === 0) {
-                    // Arrêter l'intervalle lorsque le temps est écoulé
-                    clearInterval(timerInterval);
-                }
-            }, 1000);
-          }
+            const timerElement = document.getElementById("timer");
+            boucle++;
+            start();
+        }
+    }, 1000);
+}
 
 
 
-
-  //https://www.commentcoder.com/timer-javascript/
+//recharge la page : location.reload();
+//https://www.commentcoder.com/timer-javascript/
